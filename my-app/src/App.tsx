d510c3a8bf5d369893a1196a716022bace2e24f5
larryatem <115383346+larryatem@users.noreply.github.com>
2023-10-18 17:14:59 -0500

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,51 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Counter } from './containers';
+import Logo from './logo.svg';
+
+const App = (): JSX.Element => {
+  return (
+    <div className="bg-white text-center">
+      <header className="app-header">
+        <Logo className="app-logo" title="logo" />
+        <Routes>
+          <Route path="/" element={<Counter />} />
+        </Routes>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="app-link"
+            href="https://react.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="app-link"
+            href="https://redux.js.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="app-link"
+            href="https://sabertazimi.github.io/bod"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bod App
+          </a>
+        </span>
+      </header>
+    </div>
+  );
+};
+
+export default App;
